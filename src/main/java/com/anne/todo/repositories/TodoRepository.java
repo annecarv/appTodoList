f2b1145675f60387e716754768887930cb10f713
annetherezaprofissional@gmai.com <annetherezaprofissional@gmail.com>
2022-06-01 21:05:16 -0300

    chore: Metodo @GET findById para Todo - 09

---
+++ src/main/java/com/anne/todo/repositories/TodoRepository.java
@@ -0,0 +1,17 @@
+package com.anne.todo.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.anne.todo.domain.Todo;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, Integer>{
+
+	@Query("SELECT obj FROM Todo obj WHERE obj.finalizado = false ORDER BY obj.dataParaFinalizar")
+	List<Todo> findAllOpen();
+
+}
---
+++ src/main/java/com/anne/todo/services/DBService.java
@@ -0,0 +1,34 @@
+package com.anne.todo.services;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.anne.todo.domain.Todo;
+import com.anne.todo.repositories.TodoRepository;
+
+@Service
+public class DBService {
+
+	@Autowired
+	private TodoRepository todoRepository;
+
+	public void instanciaBaseDeDados() {
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+		Todo t1 = new Todo(null, "Estudar", "Estudar Spring Boot 2 e Angular 11",
+				LocalDateTime.parse("25/03/2022 10:40", formatter), false);
+		Todo t2 = new Todo(null, "Ler", "Ouvir Música",
+				LocalDateTime.parse("25/03/2022 10:40", formatter), true);
+		Todo t3 = new Todo(null, "Dançar", "Ir pra academia",
+				LocalDateTime.parse("25/03/2022 10:40", formatter), false);
+		Todo t4 = new Todo(null, "Comer uma banana", "Brincar com a gata",
+				LocalDateTime.parse("25/03/2022 10:40", formatter), false);
+
+		todoRepository.saveAll(Arrays.asList(t1, t2, t3, t4));
+	}
+}
